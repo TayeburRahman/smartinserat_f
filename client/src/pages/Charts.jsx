990f05b40d883ea7f2c2cc8de0ad79382fa06ddc
TayeburRahman <tayebrayhan101+@gmail.com>
2025-04-24 23:00:40 +0600

    updates all the codes 7-9.38

--- client/src/pages/Charts.jsx
+++ client/src/pages/Charts.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import ChartCard from '../components/Chart/ChartCard'
-import { Doughnut, Line, Bar } from 'react-chartjs-2'
-import ChartLegend from '../components/Chart/ChartLegend'
+// import ChartCard from '../components/Chart/ChartCard'
+// import { Doughnut, Line, Bar } from 'react-chartjs-2'
+// import ChartLegend from '../components/Chart/ChartLegend'
 import PageTitle from '../components/Typography/PageTitle'
-import {
-  doughnutOptions,
-  lineOptions,
-  barOptions,
-  doughnutLegends,
-  lineLegends,
-  barLegends,
-} from '../utils/demo/chartsData'
+// import {
+//   doughnutOptions,
+//   lineOptions,
+//   barOptions,
+//   doughnutLegends,
+//   lineLegends,
+//   barLegends,
+// } from '../utils/demo/chartsData'
 
 function Charts() {
   return (
@@ -19,7 +19,7 @@ function Charts() {
       <PageTitle>Charts</PageTitle>
 
       <div className="grid gap-6 mb-8 md:grid-cols-2">
-        <ChartCard title="Doughnut">
+        {/* <ChartCard title="Doughnut">
           <div className="flex justify-center w-full">
             <div className="w-1/2">
               <Doughnut {...doughnutOptions} />
@@ -36,7 +36,7 @@ function Charts() {
         <ChartCard title="Bars">
           <Bar {...barOptions} />
           <ChartLegend legends={barLegends} />
-        </ChartCard>
+        </ChartCard> */}
       </div>
     </>
   )
