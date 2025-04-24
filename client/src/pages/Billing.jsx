5b4963ca8c7dc0c1a5fd58d03d6dc7d4f1c63427
TayeburRahman <tayebrayhan101+@gmail.com>
2025-04-24 22:57:17 +0600

    updates all the codes 7-9.38

--- client/src/pages/Billing.jsx
+++ client/src/pages/Billing.jsx
@@ -51,153 +51,153 @@ function Products({ listData, uniqId, enabled, PricingCardCallback }) {
   );
 }
 
-const Billing = ({ listData, setListData }) => {
-  const stripe = useStripe();
-  const { openSnackbar, closeSnackbar } = useContext(SnackbarContext);
-  // const { user, setUser } = useContext(AuthContext)
-  const [editBillingDetails, setEditBillingDetails] = useState(false);
-  const [enabled, setEnabled] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    if (enabled) {
-      closeSnackbar();
-    } else {
-      openSnackbar("Updating subscription..");
-    }
-  }, [enabled, openSnackbar, closeSnackbar]);
+// const Billing = ({ listData, setListData }) => {
+//   const stripe = useStripe();
+//   const { openSnackbar, closeSnackbar } = useContext(SnackbarContext);
+//   // const { user, setUser } = useContext(AuthContext)
+//   const [editBillingDetails, setEditBillingDetails] = useState(false);
+//   const [enabled, setEnabled] = useState(true);
+//   const [error, setError] = useState(null);
+//   useEffect(() => {
+//     if (enabled) {
+//       closeSnackbar();
+//     } else {
+//       openSnackbar("Updating subscription..");
+//     }
+//   }, [enabled, openSnackbar, closeSnackbar]);
 
-  const handlePaymentThatRequiresCustomerAction = (subscription) => {
-    if (!subscription) {
-      return;
-    }
-    if (subscription && subscription.status === "active") {
-      return subscription;
-    }
-    const paymentIntent = subscription.latest_invoice.payment_intent;
-    if (paymentIntent.status === "requires_action") {
-      return stripe
-        .confirmCardPayment(paymentIntent.client_secret, {
-          payment_method: listData.stripePaymentMethod.id,
-        })
-        .then((result) => {
-          if (result.error) {
-            throw Object.assign(new Error(result.error.message), {
-              response: { data: { message: result.error.message } },
-            });
-          } else {
-            if (result.paymentIntent.status === "succeeded") {
-              return subscription;
-            } else {
-              throw Object.assign(new Error("Some error occured"));
-            }
-          }
-        })
-        .catch((error) => {
-          stripeService.deleteSubscription(subscription.id);
-          throw error;
-        });
-    } else {
-      return subscription;
-    }
-  };
+//   const handlePaymentThatRequiresCustomerAction = (subscription) => {
+//     if (!subscription) {
+//       return;
+//     }
+//     if (subscription && subscription.status === "active") {
+//       return subscription;
+//     }
+//     const paymentIntent = subscription.latest_invoice.payment_intent;
+//     if (paymentIntent.status === "requires_action") {
+//       return stripe
+//         .confirmCardPayment(paymentIntent.client_secret, {
+//           payment_method: listData.stripePaymentMethod.id,
+//         })
+//         .then((result) => {
+//           if (result.error) {
+//             throw Object.assign(new Error(result.error.message), {
+//               response: { data: { message: result.error.message } },
+//             });
+//           } else {
+//             if (result.paymentIntent.status === "succeeded") {
+//               return subscription;
+//             } else {
+//               throw Object.assign(new Error("Some error occured"));
+//             }
+//           }
+//         })
+//         .catch((error) => {
+//           stripeService.deleteSubscription(subscription.id);
+//           throw error;
+//         });
+//     } else {
+//       return subscription;
+//     }
+//   };
 
-  const onSubscriptionComplete = (subscription, uniqId) => {
-    if (!subscription) return;
-    if (subscription.status === "active") {
-      return stripeService.completeSubscription(
-        subscription.id,
-        subscription.items.data[0].price.product,
-        uniqId
-      );
-    } else {
-      throw Object.assign(new Error("Some error occured"));
-    }
-  };
-  const [paymentDetails, setpaymentDetails] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(false);
-  const handleSubscription = (type, uniqId, stripeId) => {
-    if (paymentDetails) {
-      setErrorMessage(false);
-      setEnabled(false);
-      setError(null);
-      stripeService
-        .createSubscription(type, uniqId, stripeId)
-        .then(handlePaymentThatRequiresCustomerAction)
-        .then((subscription) => onSubscriptionComplete(subscription, uniqId))
-        .then(() => {
-          setListData((listData) => {
-            const newUser = {
-              ...listData,
-              subscription: { subscriptionType: type },
-            };
-            setEnabled(true);
-            return newUser;
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-          if (error.response) setError(error.response.data.message);
-          else setError("Some error occured.");
-          setEnabled(true);
-        });
-    } else {
-      setErrorMessage(true);
-    }
-  };
+//   const onSubscriptionComplete = (subscription, uniqId) => {
+//     if (!subscription) return;
+//     if (subscription.status === "active") {
+//       return stripeService.completeSubscription(
+//         subscription.id,
+//         subscription.items.data[0].price.product,
+//         uniqId
+//       );
+//     } else {
+//       throw Object.assign(new Error("Some error occured"));
+//     }
+//   };
+//   const [paymentDetails, setpaymentDetails] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState(false);
+//   const handleSubscription = (type, uniqId, stripeId) => {
+//     if (paymentDetails) {
+//       setErrorMessage(false);
+//       setEnabled(false);
+//       setError(null);
+//       stripeService
+//         .createSubscription(type, uniqId, stripeId)
+//         .then(handlePaymentThatRequiresCustomerAction)
+//         .then((subscription) => onSubscriptionComplete(subscription, uniqId))
+//         .then(() => {
+//           setListData((listData) => {
+//             const newUser = {
+//               ...listData,
+//               subscription: { subscriptionType: type },
+//             };
+//             setEnabled(true);
+//             return newUser;
+//           });
+//         })
+//         .catch((error) => {
+//           console.log(error);
+//           if (error.response) setError(error.response.data.message);
+//           else setError("Some error occured.");
+//           setEnabled(true);
+//         });
+//     } else {
+//       setErrorMessage(true);
+//     }
+//   };
 
-  const billingFormCallback = (userList) => {
-    setpaymentDetails(true);
-    setErrorMessage(false);
-    setListData(userList);
-    setEditBillingDetails(false);
-  };
-  const BillingDetailsCardCallback = () => {
-    setEditBillingDetails(true);
-  };
+//   const billingFormCallback = (userList) => {
+//     setpaymentDetails(true);
+//     setErrorMessage(false);
+//     setListData(userList);
+//     setEditBillingDetails(false);
+//   };
+//   const BillingDetailsCardCallback = () => {
+//     setEditBillingDetails(true);
+//   };
 
-  const PricingCardCallback = (type, uniqId, stripeId) => {
-    handleSubscription(type, uniqId, stripeId);
-  };
+//   const PricingCardCallback = (type, uniqId, stripeId) => {
+//     handleSubscription(type, uniqId, stripeId);
+//   };
 
-  if (!listData) {
-    return <PageError message="Some error occured : please try again." />;
-  }
+//   if (!listData) {
+//     return <PageError message="Some error occured : please try again." />;
+//   }
 
-  return (
-    <>
-      <SectionTitle>Billing Details</SectionTitle>
-      {listData.stripePaymentMethod && !editBillingDetails && (
-        <BillingDetailsCard
-          listData={listData}
-          callback={BillingDetailsCardCallback}
-        />
-      )}
-      {(!listData.stripePaymentMethod || editBillingDetails) && (
-        <BillingForm uniqId={listData.uniqId} callback={billingFormCallback} />
-      )}
-      {listData.stripePaymentMethod && !editBillingDetails && (
-        <div>
-          <PageTitle>Billing</PageTitle>
-          <SectionTitle>Plans</SectionTitle>
-          <Products
-            listData={listData}
-            uniqId={listData.uniqId}
-            enabled={enabled}
-            PricingCardCallback={PricingCardCallback}
-          />
-          {error && (
-            <HelperText valid={false} className="mb-8 text-sm">
-              {error}
-            </HelperText>
-          )}
-          {errorMessage && (
-            <HelperText valid={false} className="mb-8 text-sm">
-              provide Billing details
-            </HelperText>
-          )}
-        </div>
-      )}
-    </>
-  );
-};
+//   return (
+//     <>
+//       <SectionTitle>Billing Details</SectionTitle>
+//       {listData.stripePaymentMethod && !editBillingDetails && (
+//         <BillingDetailsCard
+//           listData={listData}
+//           callback={BillingDetailsCardCallback}
+//         />
+//       )}
+//       {(!listData.stripePaymentMethod || editBillingDetails) && (
+//         <BillingForm uniqId={listData.uniqId} callback={billingFormCallback} />
+//       )}
+//       {listData.stripePaymentMethod && !editBillingDetails && (
+//         <div>
+//           <PageTitle>Billing</PageTitle>
+//           <SectionTitle>Plans</SectionTitle>
+//           <Products
+//             listData={listData}
+//             uniqId={listData.uniqId}
+//             enabled={enabled}
+//             PricingCardCallback={PricingCardCallback}
+//           />
+//           {error && (
+//             <HelperText valid={false} className="mb-8 text-sm">
+//               {error}
+//             </HelperText>
+//           )}
+//           {errorMessage && (
+//             <HelperText valid={false} className="mb-8 text-sm">
+//               provide Billing details
+//             </HelperText>
+//           )}
+//         </div>
+//       )}
+//     </>
+//   );
+// };
 //export default Billing;
