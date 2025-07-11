db87fc00ecfe56ef03a0c9011e5e9984d9a240d3
Tayebur Rahman <86849105+TayeburRahman@users.noreply.github.com>
2025-07-11 17:30:19 +0600

    updates all

--- client/src/Firebase/ConigStorage.jsx
+++ client/src/Firebase/ConigStorage.jsx
@@ -6,18 +6,20 @@ const app = initializeApp({
     apiKey: "AIzaSyBCO67_x0QNiiRHnC2kB51FUDdWLlhcyKM",
     authDomain: "smartinserat-77dcb.firebaseapp.com",
     projectId: "smartinserat-77dcb",
-    storageBucket: "smartinserat-77dcb.firebasestorage.app",
+    storageBucket: "smartinserat-77dcb.appspot.com",
     messagingSenderId: "451114753410",
     appId: "1:451114753410:web:9b72cb064ffd2968ca839b",
-    measurementId: "G-WF8X6YEYST"
-    // apiKey: "AIzaSyAEfPW9TPvmIPEgsUAoNuQjMtmHleAcozw",
+    measurementId: "G-WF8X6YEYST" 
+});
+
+const storage = getStorage(app);
+export default storage;
+
+
+ // apiKey: "AIzaSyAEfPW9TPvmIPEgsUAoNuQjMtmHleAcozw",
     // authDomain: "job-onboard-client.firebaseapp.com",
     // projectId: "job-onboard-client",
     // storageBucket: "job-onboard-client.firebasestorage.app",
     // messagingSenderId: "843588075785",
     // appId: "1:843588075785:web:cd6d46453bbfb88045d3d8",
     // measurementId: "G-3DBT0N41CD"
-});
-
-const storage = getStorage(app);
-export default storage;
