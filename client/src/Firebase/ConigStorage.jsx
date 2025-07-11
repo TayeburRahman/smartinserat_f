5e1605faff62007c5b0ffb25afad4a9179070cda
Tayebur Rahman <86849105+TayeburRahman@users.noreply.github.com>
2025-07-11 17:12:24 +0600

    updates all

--- client/src/Firebase/ConigStorage.jsx
+++ client/src/Firebase/ConigStorage.jsx
@@ -3,13 +3,20 @@ import { getStorage } from "firebase/storage";
 
 // Initialize Firebase
 const app = initializeApp({ 
-    apiKey: "AIzaSyAEfPW9TPvmIPEgsUAoNuQjMtmHleAcozw",
-    authDomain: "job-onboard-client.firebaseapp.com",
-    projectId: "job-onboard-client",
-    storageBucket: "job-onboard-client.firebasestorage.app",
-    messagingSenderId: "843588075785",
-    appId: "1:843588075785:web:cd6d46453bbfb88045d3d8",
-    measurementId: "G-3DBT0N41CD"
+    apiKey: "AIzaSyBCO67_x0QNiiRHnC2kB51FUDdWLlhcyKM",
+    authDomain: "smartinserat-77dcb.firebaseapp.com",
+    projectId: "smartinserat-77dcb",
+    storageBucket: "smartinserat-77dcb.firebasestorage.app",
+    messagingSenderId: "451114753410",
+    appId: "1:451114753410:web:9b72cb064ffd2968ca839b",
+    measurementId: "G-WF8X6YEYST"
+    // apiKey: "AIzaSyAEfPW9TPvmIPEgsUAoNuQjMtmHleAcozw",
+    // authDomain: "job-onboard-client.firebaseapp.com",
+    // projectId: "job-onboard-client",
+    // storageBucket: "job-onboard-client.firebasestorage.app",
+    // messagingSenderId: "843588075785",
+    // appId: "1:843588075785:web:cd6d46453bbfb88045d3d8",
+    // measurementId: "G-3DBT0N41CD"
 });
 
 const storage = getStorage(app);
