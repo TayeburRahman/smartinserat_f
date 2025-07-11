d349114b0929e791bff42557724f31b4952e4b52
Tayebur Rahman <86849105+TayeburRahman@users.noreply.github.com>
2025-07-12 00:44:47 +0600

    updates all the codes

--- client/src/components/Forms/stepForm/Submit.jsx
+++ client/src/components/Forms/stepForm/Submit.jsx
@@ -16,7 +16,7 @@ import { dictionary } from '../../../resources/multiLanguages';
 import 'react-tabs/style/react-tabs.css';
 
 // console.log('=========================',process.env.REACT_APP_STRIPE_PUBLIC_KEY);  
-const stripePromise = loadStripe('pk_test_51QX5j82MtPL2aPEKFGOOrrMZ1BtMjXtqN00FHmQpuvgIsMk6iromQ67GeTJL7hwca9Jzv5xa2efR4yKiXeS6E3pB0076WqrjSM');
+const stripePromise = loadStripe('pk_live_51RjhVpE9UtRbGddx49bM4oU9ChopoNnXX3Bvv0HW7x7S9ifRoqeQKFtPzc8Ys9D6naTFLE5Kz12y2XQRG8TnXhFR004Ad13iir');
 
 function Products({ products, listData, enabled, PricingCardCallback, pages, subscriptionDuration }) {
   const languageReducer= "de";
