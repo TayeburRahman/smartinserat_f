5033b24c011e3e0f43c5d8ddbeefbc70abb73ed9
Tayebur Rahman <86849105+TayeburRahman@users.noreply.github.com>
2025-07-12 19:31:01 +0600

    updates all

--- client/src/components/Forms/stepForm/Submit.jsx
+++ client/src/components/Forms/stepForm/Submit.jsx
@@ -20,7 +20,7 @@ const stripePromise = loadStripe(config.stripe.publicKey);
 // const stripePromise = loadStripe('pk_live_51RjhVpE9UtRbGddx49bM4oU9ChopoNnXX3Bvv0HW7x7S9ifRoqeQKFtPzc8Ys9D6naTFLE5Kz12y2XQRG8TnXhFR004Ad13iir');
 
 // const stripePromise = loadStripe('pk_test_51QX5j82MtPL2aPEKFGOOrrMZ1BtMjXtqN00FHmQpuvgIsMk6iromQ67GeTJL7hwca9Jzv5xa2efR4yKiXeS6E3pB0076WqrjSM');
-
+console.log('listData=================', config.stripe.publicKey)
 
 function Products({ products, listData, enabled, PricingCardCallback, pages, subscriptionDuration }) {
   const languageReducer= "de";
