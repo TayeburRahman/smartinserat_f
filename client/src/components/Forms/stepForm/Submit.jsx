9dd7cd98a27b754f06a7588ef63295ab11abcf75
Tayebur Rahman <86849105+TayeburRahman@users.noreply.github.com>
2025-07-12 19:24:49 +0600

    updates all

--- client/src/components/Forms/stepForm/Submit.jsx
+++ client/src/components/Forms/stepForm/Submit.jsx
@@ -16,8 +16,8 @@ import { dictionary } from '../../../resources/multiLanguages';
 import 'react-tabs/style/react-tabs.css';
 
  
-// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY); 
-const stripePromise = loadStripe('pk_live_51RjhVpE9UtRbGddx49bM4oU9ChopoNnXX3Bvv0HW7x7S9ifRoqeQKFtPzc8Ys9D6naTFLE5Kz12y2XQRG8TnXhFR004Ad13iir');
+const stripePromise = loadStripe(config.stripe.publicKey); 
+// const stripePromise = loadStripe('pk_live_51RjhVpE9UtRbGddx49bM4oU9ChopoNnXX3Bvv0HW7x7S9ifRoqeQKFtPzc8Ys9D6naTFLE5Kz12y2XQRG8TnXhFR004Ad13iir');
 
 // const stripePromise = loadStripe('pk_test_51QX5j82MtPL2aPEKFGOOrrMZ1BtMjXtqN00FHmQpuvgIsMk6iromQ67GeTJL7hwca9Jzv5xa2efR4yKiXeS6E3pB0076WqrjSM');
 
